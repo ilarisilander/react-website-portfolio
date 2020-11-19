353daaa1ae35efcbdd4768eab8453c931ac48d01
ilarisilander <ilarisilander@hotmail.com>
2020-11-20 00:01:08 +0100

    Remove standard files and unnecessary code

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
